82ce4acfc7de332e17b2bfe12804f91f05e13a62
pmartin <contact@elol.fr>
2016-02-20 00:02:53 +0100

    [porrtal] demo with ng2-bootstrap

--- portal/htdocs/app/components/home/home.ts
+++ portal/htdocs/app/components/home/home.ts
@@ -1,18 +1,16 @@
 import {Component} from 'angular2/core';
 import {Router} from 'angular2/router';
 
+import {Collapse} from 'ng2-bootstrap/ng2-bootstrap';
+
 import {UserService} from '../../services/user';
 import {I18nService} from '../../services/i18n';
 
 @Component({
     selector: 'home-cmp',
-    template: `
-	<div class="home">
-	<h1>{{i18n.t('auth.logged_in_as')}} {{getUser()}}</h1>
-	<p><a (click)="logout()">Logout</a></p>
-	</div>
-	`,
-    providers: [UserService]
+    templateUrl: './app/components/home/home.html',
+    providers: [UserService],
+    directives: [Collapse]
 })
 export class HomeCmp {
 
