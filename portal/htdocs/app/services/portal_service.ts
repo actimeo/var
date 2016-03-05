57659862862150da127db03aa1bdce8e57afda63
pmartin <contact@elol.fr>
2016-03-05 16:16:48 +0100

    [portal] debug portal service error

--- portal/htdocs/app/services/portal_service.ts
+++ portal/htdocs/app/services/portal_service.ts
@@ -19,7 +19,7 @@ export class PortalService {
     listEntities() {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'entity_list', { })
-		.then(resolve);
+		.then(resolve, reject);
 	});
     }
 
@@ -27,126 +27,126 @@ export class PortalService {
 	console.log("portalService listPortals, token="+this.token);
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'portal_list', { 'prm_token': this.token })
-		.then(resolve);
+		.then(resolve, reject);
 	});
     }
 
     addPortal(name) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'portal_add', { prm_token: this.token, prm_name: name })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     renamePortal(id, name) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'portal_rename', { prm_token: this.token, prm_id: id, prm_name: name })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     deletePortal(id) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'portal_delete', { prm_token: this.token, prm_id: id })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     listMainsections(por_id) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'mainsection_list', { 'prm_token': this.token, 'prm_por_id': por_id })
-		.then(resolve);
+		.then(resolve, reject);
 	});
     }
 
     addMainsection(por_id, name) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'mainsection_add', { prm_token: this.token, prm_por_id: por_id, prm_name: name })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     deleteMainsection(id) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'mainsection_delete', { prm_token: this.token, prm_id: id })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     listPersonsections(por_id, entity) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'personsection_list', { 'prm_token': this.token, 'prm_por_id': por_id, 'prm_entity': entity })
-		.then(resolve);
+		.then(resolve, reject);
 	});
     }
 
     addPersonsection(por_id, entity, name) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'personsection_add', { prm_token: this.token, prm_por_id: por_id, prm_entity: entity, prm_name: name })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     deletePersonsection(id) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'personsection_delete', { prm_token: this.token, prm_id: id })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     listPersonmenus(pse_id) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'personmenu_list', { 'prm_token': this.token, 'prm_pse_id': pse_id })
-		.then(resolve);
+		.then(resolve, reject);
 	});
     }
 
     addPersonmenu(pse_id, name) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'personmenu_add', { prm_token: this.token, prm_pse_id: pse_id, prm_name: name })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     deletePersonmenu(id) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'personmenu_delete', { prm_token: this.token, prm_id: id })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     renamePersonmenu(id, name) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'personmenu_rename', { prm_token: this.token, prm_id: id, prm_name: name })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     listMainmenus(mse_id) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'mainmenu_list', { 'prm_token': this.token, 'prm_mse_id': mse_id })
-		.then(resolve);
+		.then(resolve, reject);
 	});
     }
 
     addMainmenu(mse_id, name) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'mainmenu_add', { prm_token: this.token, prm_mse_id: mse_id, prm_name: name })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     deleteMainmenu(id) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'mainmenu_delete', { prm_token: this.token, prm_id: id })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 
     renameMainmenu(id, name) {
 	return new Promise((resolve, reject) => {
 	    PgProc('/portal/ajax/', 'portal', 'mainmenu_rename', { prm_token: this.token, prm_id: id, prm_name: name })
-		.then(resolve);
+		.then(resolve, reject);
 	});	
     }
 }
