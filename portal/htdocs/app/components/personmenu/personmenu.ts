67d5c72300bf2bd19c8313f757ee1426c293ecd2
pmartin <contact@elol.fr>
2016-03-06 08:59:17 +0100

    [portal] alerts on person menu edition

--- portal/htdocs/app/components/personmenu/personmenu.ts
+++ portal/htdocs/app/components/personmenu/personmenu.ts
@@ -2,6 +2,7 @@ import {Component,Directive, Input, Output, EventEmitter, Inject, ElementRef} fr
 
 import {PortalService} from './../../services/portal_service';
 import {I18nService} from '../../services/i18n';
+import {AlertsService} from '../../services/alerts';
 
 import {TOOLTIP_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
@@ -36,7 +37,8 @@ export class Personmenu {
     @Output() onchange: EventEmitter<void> = new EventEmitter<void>();
 
     constructor(private _portalService: PortalService,
-		private i18n: I18nService
+		private i18n: I18nService,
+		private alerts: AlertsService
 	       ) {
 	this.viewcfg = true;
 	this.viewtools = false;
@@ -71,16 +73,20 @@ export class Personmenu {
     doRename() {
 	this._portalService.renamePersonmenu(this.menu.pme_id, this.menu.pme_name).then(data => {
 	    this.onchange.emit(null);	
+	    this.alerts.success(this.i18n.t('portal.alerts.personmenu_renamed'));
 	}).catch(err => {
 	    console.log("err "+err);
+	    this.alerts.danger(this.i18n.t('portal.alerts.error_renaming_personmenu'));
 	});		    
     }
     
     onDelete() {
 	this._portalService.deletePersonmenu(this.menu.pme_id).then(data => {
 	    this.onchange.emit(null);	
+	    this.alerts.success(this.i18n.t('portal.alerts.personmenu_deleted'));
 	}).catch(err => {
 	    console.log("err "+err);
+	    this.alerts.danger(this.i18n.t('portal.alerts.error_deleting_personmenu'));
 	});		    
     }
 }
--- portal/htdocs/app/components/personmenu_add/personmenu_add.ts
+++ portal/htdocs/app/components/personmenu_add/personmenu_add.ts
@@ -4,6 +4,7 @@ import { BUTTON_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 
 import {PortalService} from './../../services/portal_service';
 import {I18nService} from '../../services/i18n';
+import {AlertsService} from '../../services/alerts';
 
 import {FocusDirective} from './../../directives/focus';
 
@@ -25,21 +26,13 @@ export class PersonmenuAdd {
     menuname_focused: boolean;
 
     constructor(private _portalService: PortalService,
-		private i18n: I18nService
+		private i18n: I18nService,
+		private alerts: AlertsService
 	       ) {
 	this.getting_name = false;
 	this.menuname_focused = false;
     }
     
-    ngOnInit() {	    
-/*	this._portalService.listEntities().then(data => {
-	    console.log("listEntities: "+data);
-	    this.entities = data;
-	}).catch(err => {
-	    console.log("err "+err);
-	});	*/
-    }
-
     onAddMenu() {
 	this.getting_name = true;
 	this.menuname_focused = true;
@@ -57,8 +50,10 @@ export class PersonmenuAdd {
 
 	this._portalService.addPersonmenu(this.pse_id, this.menuname).then(new_pme_id => {
 	    this.onadded.emit(null);
+	    this.alerts.success(this.i18n.t('portal.alerts.personmenu_added'));
 	}).catch(err => {
 	    console.log("err "+err);
+	    this.alerts.danger(this.i18n.t('portal.alerts.error_adding_personmenu'));
 	});	
 	this.cancelAddMenu();
     }
