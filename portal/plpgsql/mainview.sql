19e46b1d1a0a93d6cc0dd788b382a339a3eae067
pmartin <contact@elol.fr>
2016-03-23 21:11:33 +0100

    [portal] mainview

---
+++ portal/plpgsql/mainview.sql
@@ -0,0 +1,60 @@
+/*
+mainview_add(mme_id, title, icon, pme_associated): integer
+mainview_delete(mme_id): void
+mainview_get: mainview
+mainview_set(mme_id, title, icon, pme_associated): void
+*/
+
+SET search_path = portal;
+
+CREATE OR REPLACE FUNCTION mainview_set(prm_token integer, prm_mme_id integer, prm_title text, prm_icon text, prm_pme_id_associated integer)
+RETURNS void
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  PERFORM login._token_assert(prm_token, '{structure}');
+  UPDATE portal.mainview SET 
+    mvi_title = prm_title, 
+    mvi_icon = prm_icon,
+    pme_id_associated = prm_pme_id_associated
+    WHERE mme_id = prm_mme_id;
+  IF NOT FOUND THEN
+    INSERT INTO portal.mainview (mme_id, mvi_title, mvi_icon, pme_id_associated) 
+      VALUES(prm_mme_id, prm_title, prm_icon, prm_pme_id_associated);
+  END IF;
+END;
+$$;
+COMMENT ON FUNCTION mainview_set(prm_token integer, prm_mme_id integer, prm_title text, prm_icon text, prm_pme_id_associated integer) IS 'Set or update information about a view attached to a specified main menu';
+
+CREATE OR REPLACE FUNCTION mainview_delete(prm_token integer, prm_mme_id integer)
+RETURNS void
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  PERFORM login._token_assert(prm_token, '{structure}');
+  DELETE FROM portal.mainview WHERE mme_id = prm_mme_id;
+  IF NOT FOUND THEN
+    RAISE EXCEPTION USING ERRCODE = 'no_data_found';
+  END IF;
+END;
+$$;
+COMMENT ON FUNCTION mainview_delete(prm_token integer, prm_mme_id integer) IS 'Delete a view attached to a main menu';
+
+CREATE OR REPLACE FUNCTION mainview_get(prm_token integer, prm_mme_id integer)
+RETURNS portal.mainview
+LANGUAGE plpgsql
+STABLE
+AS $$
+DECLARE
+  ret portal.mainview;
+BEGIN
+  PERFORM login._token_assert(prm_token, '{structure}');
+  SELECT * INTO ret FROM portal.mainview WHERE mme_id = prm_mme_id;
+  IF NOT FOUND THEN
+    RAISE EXCEPTION USING ERRCODE = 'no_data_found';
+  END IF;
+  RETURN ret;
+END;
+$$;
+COMMENT ON FUNCTION mainview_get(prm_token integer, prm_mme_id integer) IS 'Get information about a view attached to a main menu';
+
