6c9e75b138d85a94c0504b54427fe12bc895105b
pmartin <contact@elol.fr>
2016-04-01 15:52:17 +0200

    [portal] topics

---
+++ portal/tests/topicsTest.php
@@ -0,0 +1,44 @@
+<?php
+require_once '../../pgproc/php/pgprocedures.php';
+require_once '../../config.inc.php';
+
+class topicsTest extends PHPUnit_Framework_TestCase {
+  private static $base;
+  private static $pgHost;
+  private static $pgUser;
+  private static $pgPass;
+  private static $pgDatabase;
+
+  public static function setUpBeforeClass() {
+    
+    // Get connection params
+    global $pg_host, $pg_user, $pg_pass, $pg_database;
+    self::$pgHost = $pg_host;
+    self::$pgUser = $pg_user;
+    self::$pgPass = $pg_pass;
+    self::$pgDatabase = $pg_database;
+    self::assertNotNull(self::$pgHost);
+    self::assertNotNull(self::$pgUser);
+    self::assertNotNull(self::$pgPass);
+    self::assertNotNull(self::$pgDatabase);
+    
+    // Create object
+    self::$base = new PgProcedures2 (self::$pgHost, self::$pgUser, self::$pgPass, self::$pgDatabase);
+    self::assertNotNull(self::$base);    
+  }
+
+  protected function assertPreConditions()
+  {
+    self::$base->startTransaction();
+  }
+
+  protected function assertPostConditions()
+  {
+    self::$base->rollback();
+  }
+
+  public function testTopicsList() {
+    $topics = self::$base->portal->topics_list();
+    self::assertEquals('social', $topics[0]);
+  }  
+}
