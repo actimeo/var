7c472fac0c73eebe032828ce6086d4f4dad90b40
pmartin <contact@elol.fr>
2016-04-07 17:20:17 +0200

    [doc] generate schema schemas

---
+++ doc/generate.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+. ../config.sh
+
+postgresql_autodoc -d $DBNAME -h localhost -u $DBUSER --password=$DBPASS -t dia -s portal -f portal
+postgresql_autodoc -d $DBNAME -h localhost -u $DBUSER --password=$DBPASS -t dia -s 'patient|organ' -f organ_patient
+postgresql_autodoc -d $DBNAME -h localhost -u $DBUSER --password=$DBPASS  -f all
