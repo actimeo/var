e118bf6c9c83d099af496dbae6d7bfe15681940d
pmartin <contact@elol.fr>
2016-04-14 10:12:38 +0200

    [ng2-portal] set focus to username on login page

--- ng2-portal/src/app/login/login.ts
+++ ng2-portal/src/app/login/login.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, ElementRef, ViewChild, Renderer} from 'angular2/core';
 import {Router} from 'angular2/router';
 
 import {UserService} from '../services/user/user';
@@ -12,25 +12,32 @@ import {PgService} from '../services/pg-service/pg-service';
   providers: []
 })
 export class LoginCmp {
+  @ViewChild('username') username: ElementRef;
+
   errormsg: string;
 
   // We inject the router via DI
   constructor(
-      private router: Router, private userService: UserService, private i18n: I18nService,
-      private pgService: PgService) {}
+    private router: Router, private userService: UserService, private i18n: I18nService,
+    private pgService: PgService, private renderer: Renderer) { }
+
+  ngAfterViewInit() {
+    // set focus to username
+    this.renderer.invokeElementMethod(this.username.nativeElement, 'focus', null);
+  }
 
   login(event, username, password) {
     event.preventDefault();
     this.pgService
-        .pgcall(
-            'login', 'user_login',
-            {'prm_login': username, 'prm_pwd': password, prm_rights: ['structure']})
-        .then((data: any) => {
-          this.userService.connect(data.usr_token, username);
-          this.errormsg = null;
-          this.router.parent.navigateByUrl('/home');
-        })
-        .catch(error => { this.errormsg = this.i18n.t('auth.autherror'); });
+      .pgcall(
+      'login', 'user_login',
+      { 'prm_login': username, 'prm_pwd': password, prm_rights: ['structure'] })
+      .then((data: any) => {
+        this.userService.connect(data.usr_token, username);
+        this.errormsg = null;
+        this.router.parent.navigateByUrl('/home');
+      })
+      .catch(error => { this.errormsg = this.i18n.t('auth.autherror'); });
 
     // We call our API to log the user in. The username and password are entered
     // by the user
