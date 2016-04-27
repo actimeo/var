d35dc58061666508f5fe48fd1453fea80b8b0a2c
pmartin <contact@elol.fr>
2016-04-27 14:26:03 +0200

    [login] give all rights to Variation user

--- login/sql/auth.sql
+++ login/sql/auth.sql
@@ -18,7 +18,7 @@ CREATE TABLE login."user" (
   usr_token_creation_date timestamp with time zone
 );
 
-INSERT INTO login.user(usr_login, usr_salt, usr_rights) values ('variation', pgcrypto.crypt('variation', pgcrypto.gen_salt('bf', 8)), '{users}');
+INSERT INTO login.user(usr_login, usr_salt, usr_rights) values ('variation', pgcrypto.crypt('variation', pgcrypto.gen_salt('bf', 8)), '{users,structure,organization}');
 INSERT INTO login.user(usr_login, usr_salt, usr_rights) values ('portaluser', pgcrypto.crypt('portal/user', pgcrypto.gen_salt('bf', 8)), '{structure}');
 INSERT INTO login.user(usr_login, usr_salt, usr_rights) values ('organuser', pgcrypto.crypt('organ/user', pgcrypto.gen_salt('bf', 8)), '{organization}');
 
