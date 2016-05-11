e9dc903977b95c06f6ef9d320c0742f042856cab
pmartin <contact@elol.fr>
2016-05-11 17:27:40 +0200

    [ng2-portal] Upgarde to ng2-i18next 0.0.10

--- ng2-portal/src/app.ts
+++ ng2-portal/src/app.ts
@@ -4,7 +4,7 @@ import {ROUTER_PROVIDERS} from 'angular2/router';
 import {LocationStrategy, HashLocationStrategy} from 'angular2/platform/common';
 import {HTTP_PROVIDERS} from 'angular2/http';
 
-import {I18nService} from 'ng2-i18next/ng2-i18next';
+import {I18nServiceConfig, I18nService} from 'ng2-i18next/ng2-i18next';
 import {AlertsService, FootertipService} from 'variation-toolkit/variation-toolkit';
 
 import {Ng2PortalApp} from './app/ng2-portal';
@@ -13,13 +13,29 @@ import {
   PgService, PgServiceConfig} from 'ng2-postgresql-procedures/ng2-postgresql-procedures';
 import {SelectedMenus} from './app/services/selected-menus/selected-menus';
 
+declare var i18nextBrowserLanguageDetector: any;
+declare var i18nextXHRBackend: any;
+
 //import {enableProdMode} from 'angular2/core';
 //enableProdMode();
+
+const I18N_PROVIDERS = [
+    provide(I18nServiceConfig, {
+        useValue: {
+            use: [i18nextBrowserLanguageDetector, i18nextXHRBackend],
+            config: {
+                detection: {order: ['navigator']},
+                fallbackLng: 'en'
+            }
+        }
+    }),
+    I18nService
+];
 
 bootstrap(Ng2PortalApp, [
   provide(PgServiceConfig, { useValue: { pgPath: '/pg', prmTokenName: 'prm_token' } }),
   ROUTER_PROVIDERS, HTTP_PROVIDERS,
-  UserService, I18nService,
+  UserService, I18N_PROVIDERS,
   PgService, FootertipService,
   AlertsService, SelectedMenus, provide(LocationStrategy, { useClass: HashLocationStrategy })
 ]);
