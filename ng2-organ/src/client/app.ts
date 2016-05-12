8328578c3157bf257420ab540ae10d339617a0f5
pmartin <contact@elol.fr>
2016-05-12 11:24:35 +0200

    [ng2-organ] upgrade to ng2-i18next 0.0.10

--- ng2-organ/src/client/app.ts
+++ ng2-organ/src/client/app.ts
@@ -4,7 +4,7 @@ import {ROUTER_PROVIDERS} from 'angular2/router';
 import {LocationStrategy, HashLocationStrategy} from 'angular2/platform/common';
 import {HTTP_PROVIDERS} from 'angular2/http';
 
-import {I18nService} from 'ng2-i18next/ng2-i18next';
+import {I18nServiceConfig, I18nService} from 'ng2-i18next/ng2-i18next';
 import {AlertsService, FootertipService} from 'variation-toolkit/variation-toolkit';
 import {
   UserService,
@@ -12,8 +12,24 @@ import {
 
 import {Ng2OrganApp} from './app/ng2-organ';
 
+declare var i18nextBrowserLanguageDetector: any;
+declare var i18nextXHRBackend: any;
+
+const I18N_PROVIDERS = [
+    provide(I18nServiceConfig, {
+        useValue: {
+            use: [i18nextBrowserLanguageDetector, i18nextXHRBackend],
+            config: {
+                detection: {order: ['navigator']},
+                fallbackLng: 'en'
+            }
+        }
+    }),
+    I18nService
+];
+
 bootstrap(Ng2OrganApp, [
-  HTTP_PROVIDERS,
+  HTTP_PROVIDERS, I18N_PROVIDERS,
   provide(PgServiceConfig, { useValue: { pgPath: '/pg', prmTokenName: 'prm_token' } }),
   UserService, PgService, FootertipService, AlertsService, I18nService,
   ROUTER_PROVIDERS, provide(LocationStrategy, { useClass: HashLocationStrategy })]);
