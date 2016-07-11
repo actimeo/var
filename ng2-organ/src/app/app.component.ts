53b9e77888cd3f1e12809f638a6ef622638ac84f
pmartin <contact@elol.fr>
2016-07-11 12:28:43 +0200

    [ng2-organ] precompile

--- ng2-organ/src/app/app.component.ts
+++ ng2-organ/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
+import { HomeComponent } from './home';
+import { HomeSystemComponent } from './home-system';
+import { HomeOrganizationsComponent } from './home-organizations';
+import { HomePortalsComponent } from './home-portals';
 import { DataProviderService } from './data-provider.service';
 
 import 'primeuiNgAll';
@@ -11,7 +15,8 @@ import 'primeuiNgAll';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
   directives: [ROUTER_DIRECTIVES],
-  providers: [DataProviderService]
+  providers: [DataProviderService],
+  precompile: [HomeComponent, HomePortalsComponent, HomeOrganizationsComponent, HomeSystemComponent]
 })
 export class AppComponent {
   title = 'app works!';
